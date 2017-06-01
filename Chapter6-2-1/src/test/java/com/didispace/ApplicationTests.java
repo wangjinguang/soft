173f3c5e8128a6b49eec1e174b561c28df44842e
zhaiyongchao <zhaiyongchao>
2017-06-01 17:47:31 +0800

    init

---
+++ Chapter6-2-1/src/test/java/com/didispace/ApplicationTests.java
@@ -0,0 +1,29 @@
+package com.didispace;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ *
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+public class ApplicationTests {
+
+	private static final Log log = LogFactory.getLog(ApplicationTests.class);
+
+	@Test
+	public void test1() throws Exception {
+
+	}
+
+}
